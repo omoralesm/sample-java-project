78cd20427ae7cf09ecc4ea492dc8898db1ad3bec
omoralesm <omoralesm@gmail.com>
2021-02-01 19:25:27 -0600

    Otro sencillo
    Otro sencillo

--- src/sample/java/project/SampleJavaProject.java
+++ src/sample/java/project/SampleJavaProject.java
@@ -13,6 +13,7 @@ import lombok.Setter;
  * The main class of the application. It contains the main() method,
  * the first method called.
  * OMM
+ * Otro sencillo
  */
 @NoArgsConstructor
 @AllArgsConstructor
